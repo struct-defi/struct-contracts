129eac7636f15c5cfa10a7d00fc60d36b9467424
PraneshASP <praneshas2000@gmail.com>
2024-01-09 13:23:39 +0530

    chore: cleanup

--- deployment-helpers/production/ConfigProduction.sol
+++ deployment-helpers/production/ConfigProduction.sol
@@ -3,15 +3,13 @@
 pragma solidity 0.8.11;
 
 import {Config} from "../Config.sol";
-import {IDistributionManager} from "@interfaces/IDistributionManager.sol";
-import {Script} from "forge-std/src/Script.sol";
 
 contract ConfigProduction is Config {
     uint256 internal allocatedTotalPoints = 1e3;
     uint256 internal allocationPoints = 1e3;
     uint256 internal allocationFee = 1e3;
     uint256 internal rewardsPerSec;
- 
+
     uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY_PRODUCTION");
     address deployerAddress = vm.addr(deployerPrivateKey);
 }
